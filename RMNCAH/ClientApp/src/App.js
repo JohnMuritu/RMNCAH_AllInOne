163fb2bc71fa57291e6696bf74222d8049f69711
John <john.macharia@thepalladiumgroup.com>
2021-09-14 16:39:41 +0300

    version 1 deployed

--- RMNCAH/ClientApp/src/App.js
+++ RMNCAH/ClientApp/src/App.js
@@ -19,7 +19,7 @@ const App = () => {
   const AUTH_TOKEN = useSelector((state) => state.main_reducer.userToken);
   const bearer_token = `Bearer ${AUTH_TOKEN}`;
   //axios.defaults.baseURL = 'https://localhost:5001';
-  axios.defaults.baseURL = 'http://localhost:90';
+  axios.defaults.baseURL = 'https://rmncah.southafricanorth.cloudapp.azure.com';
   axios.defaults.headers.common['Authorization'] = bearer_token;
 
   return (
